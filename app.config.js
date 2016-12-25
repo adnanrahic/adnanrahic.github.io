7a0de8c2aee4115ba4152f5ae07f49aad3149ca7
adnanrahic <ado.raha198@gmail.com>
2016-12-25 02:02:58 +0100

    fsdfds

--- app.config.js
+++ app.config.js
@@ -5,10 +5,15 @@
 		.module('app')
 		.config(config);
 	
-	config.$inject = ['$compileProvider', '$locationProvider'];
-	function config($compileProvider, $locationProvider) {
+	config.$inject = ['$compileProvider', '$locationProvider', '$urlRouterProvider'];
+	function config($compileProvider, $locationProvider, $urlRouterProvider) {
 		$compileProvider.debugInfoEnabled(false);
     	$locationProvider.html5Mode(true).hashPrefix('!');
+
+    	$urlRouterProvider.otherwise(function($injector) {
+		    var $state = $injector.get('$state');
+		    $state.go('/');
+		});
 	}
 
 
