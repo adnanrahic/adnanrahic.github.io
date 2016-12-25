5e9dfd3d7dbda1feca9b83ae26c10378ab29cb4e
adnanrahic <ado.raha198@gmail.com>
2016-12-25 01:19:10 +0100

    testing

---
+++ homeComponent/home.controller.js
@@ -0,0 +1,15 @@
+(function () {
+	"use strict"
+	
+	angular
+		.module('app')
+		.controller('homeController', homeController);
+
+	//homeController.$inject = [];
+	function homeController() {
+		var vm = this;
+
+		vm.home = "Yep.";
+	}
+	
+})();
---
+++ homeComponent/home.routes.js
@@ -0,0 +1,28 @@
+(function () {
+	"use strict"
+	
+	angular
+		.module('app')
+		.config(homeRoutes);
+
+	homeRoutes.$inject = ['$stateProvider'];
+	function homeRoutes($stateProvider) {
+		var landingState = {
+			name: 'landing',
+			url: '/',
+			templateUrl: 'homeComponent/home.html',
+			controller: 'homeController',
+			controllerAs: 'vm'
+		}
+		var homeState = {
+			name: 'home',
+			url: '/home',
+			templateUrl: 'homeComponent/home.html',
+			controller: 'homeController',
+			controllerAs: 'vm'
+		}
+  		$stateProvider.state(landingState);
+  		$stateProvider.state(homeState);
+	}
+	
+})();
