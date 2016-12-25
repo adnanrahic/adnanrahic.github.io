27dd9c5aef151b411a3d028a54b5a14708b27f50
adnanrahic <ado.raha198@gmail.com>
2016-12-25 12:32:43 +0100

    added contact

---
+++ contactComponent/contact.routes.js
@@ -0,0 +1,17 @@
+(function () {
+	"use strict"
+	
+	angular
+		.module('app')
+		.config(contactRoutes);
+
+	contactRoutes.$inject = ['$routeProvider'];
+	function contactRoutes($routeProvider) {
+		$routeProvider.when("/contact", {
+	        controller: "contactController",
+	        controllerAs: "vm",
+	        templateUrl: "contactComponent/contact.html"
+	    });
+	}
+	
+})();
