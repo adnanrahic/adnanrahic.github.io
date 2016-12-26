d2921149dbe459da11e9144ea1e80af16bee027f
adnanrahic <ado.raha198@gmail.com>
2016-12-26 01:09:04 +0100

    test

--- storyComponent/story.factory.js
+++ storyComponent/story.factory.js
@@ -17,7 +17,7 @@
 		function getStory(id) {
 			var stories = appFactory.getStories();
 			var q = $q.defer();
-			for (var i = 1; i < stories.length; i++) {
+			for (var i = 0; i < stories.length; i++) {
 				if (stories[i].id == id) {
 					q.resolve(stories[i]);
 					break;
