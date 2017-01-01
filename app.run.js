3fd10fa5721d72a7173ff614fddefb54cc218b80
adnanrahic <ado.raha198@gmail.com>
2017-01-01 21:57:32 +0100

    testing template cache

--- app.run.js
+++ app.run.js
@@ -5,17 +5,24 @@
         .module('app')
         .run(run);
 
-    run.$inject = ['$route', '$templateCache', '$http'];
-    function run($route, $templateCache, $http) {
+    run.$inject = ['$route', '$templateCache', '$http', '$rootScope'];
+    function run($route, $templateCache, $http, $rootScope) {
+        
         var url;
         for (var i in $route.routes) {
             if ($route.routes[i].preload) {
                 if (url = $route.routes[i].templateUrl) {
-                    console.log(url);
                     $http.get(url, { cache: $templateCache });
                 }
             }
         }
+
+
+        $rootScope.view = false;
+        $rootScope.$on('$viewContentLoaded', function() {
+            $rootScope.view = true;
+        });
+
     }
 
 })();
